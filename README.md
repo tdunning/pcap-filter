a3192ed5fbd47fd77df17ba5789db8e40bcef553
Ted Dunning <tdunning@apache.org>
2016-06-14 00:54:06 -0700

    Clean up fast loop. Add more stuff to README.md

--- README.md
+++ README.md
@@ -17,37 +17,41 @@ difference this can make.
 Well, if you don't have just less than a minute to run the tests, here is what I 
 get on my laptop:
 
-    Speed test for per packet object
-        Read 859.7 MB in 15.03 s for 57.2 MB/s
+    Speed test for per packet object without buffering
+        Read 859.7 MB in 14.93 s for 57.6 MB/s
         8772352 packets, 8772352 TCP packets, 0 UDP
-
+    
+    Speed test for per packet object with buffering
+        Read 859.7 MB in 1.13 s for 761.1 MB/s
+        8772352 packets, 8772352 TCP packets, 0 UDP
+        
     Speed test for in-place packet decoding
-        Read 859.7 MB in 0.50 s for 1714.3 MB/s
-        8772308 packets, 8772308 TCP packets, 0 UDP
-
+        Read 859.7 MB in 0.53 s for 1611.4 MB/s
+        8772352 packets, 8772352 TCP packets, 0 UDP
+    
 The data here is in cache, not on disk. Since we want to see how fast this can go 
 without worrying about extraneous bottlenecks, this is good.
  
-The short answer is that decoding packages in a sensible way is about 30x faster.
+The short answer is that buffering and decoding packages in a sensible way is about 
+30x faster than being silly. Of this improvement, simply buffering reasonably
+gives nearly 15x improvement and in-place decoding gives the remaining 2x.
 
-You may note that the output above shows fewer packets for the faster parser. That
-happens because I didn't think it was worth going to the trouble of parsing the last
-partial buffer full of data. It isn't hard to fix this.
-
-I think, by the way, that this speedup can be explained to a large extent by better I/O
-patterns. Here are results from a different test that reads a GB using different size
+To illustrate the improvement due to better I/O patterns, here are results 
+from a different test that reads a GB of data using different size
 reads:
 
-| Read size | Total size | Time | Speed |
-| ---------: | ---------- | ----: | -----: |
-| 50| 1000.0 MB | 15.8 s: | 63.1 MB/s |
-| 100| 1000.0 MB | 7.9 s | 127.2 MB/s |
-| 200| 1000.0 MB | 4.2 s | 240.8 MB/s |
-| 500| 1000.0 MB | 1.8 s | 546.3 MB/s |
-| 1000| 1000.0 MB | 1.0 s | 985.9 MB/s |
-| 10000| 1000.0 MB | 0.4 s | 2519.4 MB/s |
+| Read size | Time | Speed |
+| ---------: | ----: | -----: |
+| 50	| 16.4 s | 60.9 MB/s  |
+| 100	| 8.7 s | 114.6 MB/s  |
+| 200	| 4.6 s | 216.6 MB/s  |
+| 500	| 2.0 s | 489.0 MB/s  |
+| 1000	| 1.1 s | 911.5 MB/s  |
+| 2000	| 0.7 s | 1456.9 MB/s |
+| 3000	| 0.6 s | 1699.6 MB/s |
+| 4000	| 0.6 s | 1709.6 MB/s |
+| 5000	| 0.4 s | 2321.1 MB/s |
+| 6000	| 0.4 s | 2497.3 MB/s |
+| 8000	| 0.5 s | 2025.8 MB/s |
+| 10000	| 0.5 s | 2191.2 MB/s |
 
-
-That looks like this with a bit more resolution on it:
-
-<img src='speed-graph.png' width=500 height=400>
--- src/test/java/com/mapr/PacketDecoderTest.java
+++ src/test/java/com/mapr/PacketDecoderTest.java
@@ -5,14 +5,7 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -63,14 +56,29 @@ public class PacketDecoderTest {
 
     /**
      * This tests the speed when creating an actual object for each packet.
-     *
-     * That isn't very fast.
+     * <p>
+     * Even with decent buffering, this isn't very fast.
      *
      * @throws IOException
      */
     @Test
     public void testConventionalApproach() throws IOException {
-        InputStream in = new FileInputStream(bigFile);
+        speedRun(new FileInputStream(bigFile), " without buffering");
+    }
+
+    /**
+     * This tests the speed when creating an actual object for each packet.
+     * <p>
+     * Even with decent buffering, this isn't very fast.
+     *
+     * @throws IOException
+     */
+    @Test
+    public void testBufferedApproach() throws IOException {
+        speedRun(new BufferedInputStream(new FileInputStream(bigFile), 100000), " with buffering");
+    }
+
+    private void speedRun(InputStream in, String msg) throws IOException {
         PacketDecoder pd = new PacketDecoder(in);
         PacketDecoder.Packet p = pd.nextPacket();
         long total = 0;
@@ -90,7 +98,7 @@ public class PacketDecoderTest {
             p = pd.nextPacket();
         }
         long t1 = System.nanoTime();
-        System.out.printf("\nSpeed test for per packet object\n");
+        System.out.printf("\nSpeed test for per packet object%s\n", msg);
         System.out.printf("    Read %.1f MB in %.2f s for %.1f MB/s\n", total / 1e6, (t1 - t0) / 1e9, (double) total * 1e3 / (t1 - t0));
         System.out.printf("    %d packets, %d TCP packets, %d UDP\n", allCount, tcpCount, udpCount);
         System.out.printf("\n\n\n");
@@ -99,6 +107,7 @@ public class PacketDecoderTest {
     /**
      * Tests speed for in-place decoding. This is enormously faster than creating objects, largely
      * because we rarely have to move any data. Instead, we can examine as it lies in the buffer.
+     *
      * @throws IOException
      */
     @Test
@@ -110,31 +119,35 @@ public class PacketDecoderTest {
         byte[] buffer = new byte[100000];
         int validBytes = in.read(buffer);
 
-        int offset = pd.decodePacket(buffer, 0, p);
+        int offset = 0;
         long total = 0;
         int tcpCount = 0;
         int udpCount = 0;
         int allCount = 0;
         long t0 = System.nanoTime();
-        while (p != null) {
+        while (offset < validBytes) {
+            // get new data and shift current data to beginning of buffer if there is any danger
+            // of straddling the buffer end in the next packet
+            // even with jumbo packets this should be enough space to guarantee parsing
+            if (validBytes - offset < 9000) {
+                System.arraycopy(buffer, 0, buffer, offset, validBytes - offset);
+                validBytes = validBytes - offset;
+                offset = 0;
+
+                int n = in.read(buffer, validBytes, buffer.length - validBytes);
+                if (n > 0) {
+                    validBytes += n;
+                }
+            }
+
+            // decode the packet as it lies
+            offset = pd.decodePacket(buffer, offset, p);
             total += p.getPacketLength();
             allCount++;
             if (p.isTcpPacket()) {
                 tcpCount++;
             } else if (p.isUdpPacket()) {
                 udpCount++;
-            }
-            offset = pd.decodePacket(buffer, offset, p);
-            if (validBytes - offset < 5000) {
-                System.arraycopy(buffer, 0, buffer, offset, validBytes - offset);
-                validBytes = validBytes - offset;
-                int n = in.read(buffer, validBytes, buffer.length - validBytes);
-                if (n <= 0) {
-                    p = null;
-                } else {
-                    validBytes += n;
-                    offset = 0;
-                }
             }
         }
         long t1 = System.nanoTime();
@@ -146,6 +159,7 @@ public class PacketDecoderTest {
 
     /**
      * Creates an ephemeral file of about a GB in size
+     *
      * @throws IOException
      */
 
@@ -172,6 +186,7 @@ public class PacketDecoderTest {
     /**
      * Compares how fast we can read a big file with different size reads. This tells
      * us what changes when we read many packets from the file at once.
+     *
      * @throws IOException
      */
     @Test
@@ -192,7 +207,6 @@ public class PacketDecoderTest {
                 double t = (t1 - t0) * 1e-9;
                 System.out.printf("%d\t%.1f MB in %.1f s, %.1f MB/s\n", size, total * 1e-6, t, total * 1e-6 / t);
             }
-
         }
         System.out.flush();
     }
